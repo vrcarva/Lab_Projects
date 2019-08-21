bf588f83702e3b614fc12f295e77acb2725c5c45
Flavio Mourao <f_agm@yahoo.com.br>
2019-08-21 15:02:32 -0300

    Update 21-08-19

--- Electronics/Arduino_Biphasic_Stimulator/Arduino_Code/Biphasic_STM/Biphasic_STM.ino
+++ Electronics/Arduino_Biphasic_Stimulator/Arduino_Code/Biphasic_STM/Biphasic_STM.ino
@@ -2,7 +2,12 @@
 Flavio Mourao
 NNC / UFMG - 6th November 2018
 
-Biphasic square waves trigger for periodic electrical stimulation 
+Biphasic square waves for periodic electrical stimulation
+
+The digital signal is generated through an Arduino with programmable attributes such as wavelengths and interpulse intervals. 
+This signal from Arduino is decoupled through a simple circuit formed by a Optocoupler and then the output amplitude (0 - 9 V) can be modulated by a potentiometer.
+
+Extra digital outputs are used to reproduce the generated time events and to allow visual feedback through an LED.
 
 */
 
