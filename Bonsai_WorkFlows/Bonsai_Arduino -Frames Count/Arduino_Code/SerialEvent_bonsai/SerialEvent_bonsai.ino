ab331b0fa845403a170af74abc278f3b5846e5cb
Flavio Mourao <f_agm@yahoo.com.br>
2020-03-26 09:09:17 -0300

    Update 26-03-2020

--- Bonsai_WorkFlows/Bonsai_Arduino -Frames Count/Arduino_Code/SerialEvent_bonsai/SerialEvent_bonsai.ino
+++ Bonsai_WorkFlows/Bonsai_Arduino -Frames Count/Arduino_Code/SerialEvent_bonsai/SerialEvent_bonsai.ino
@@ -1,36 +1,37 @@
-/* CONTADOR DE FRAMES PARA O PAREAMENTE DE REGISTROS EEG
- * Um programa feito do fundo do meu coração junto com o Flávio 
- *
- * 
- * Através do sofware BONSAI 2.3 (https://bonsai-rx.org/) recebe-se o número do "FRAME" em uma "string" de 10 dígitos (sem \r\n) em um BaudRate 
- * de 9600. Aparentemente o Bonsai não permite programar a função SerialStringWrite com
- * outro BaudRate a não ser 9600.
- * 
- * Uma saida digital (PWM) deve escolhida para "FLIP" a cada transição de nFrames. Ou seja, o pino fica nFrames
- * em alta e nFrames em baixa, oscilando a cada frame coletado e salvo.
- * 
- * Marcio Moraes e Flavio Mourao
- * No dia 24/4/2019 - NNC no Computador da filha do Mazoni
- */
+/* Núcleo de Neurociências - UFMG/Brazil (Universidade Federal de Minas Gerais)
+ *  
+ * FRAMES COUNTER TO SYNCHRONIZE VIDEOS WITH ELECTROPHYSIOLOGICAL RECORDS
 
-int nFrames=10;   // Define o numero de "frames" a ser salvo
-int iCount=0;    // Armazenará o frame coletado
-char Buffer[20]; // Buffer para o "string". Só precisa de dez, mas sou exagerado mesmo
+* Through BONSAI software (https://bonsai-rx.org/) we receive a "FRAME" number in a 10-digit "string" (without \ r \ n) in a 9600 BaudRate.
+* (Bonsai does not appear to be able to program the "SerialStringWrite" function with another BaudRate).
+*
+* A digital output (PWM) must be chosen to "FLIP" at each nFrames transition. That is, the pin is nFrames
+* high and nFrames low, oscillating at each frame collected and saved.
+
+* Authors: Flávio Afonso Gonçalves Mourão - mourao.fg@gmail.com
+*          Márcio Flávio Dutra Moraes     - mfdm@icb.ufmg.br
+*          
+*          24/04/2019
+*/
+
+int nFrames=10;   // Defines the number of frames to be saved
+int iCount=0;    // Will store the collected frame
+char Buffer[20]; // Buffer for the "string". You only need ten, but I added more just to make sure
 
 void setup()
 {
-  pinMode(10,OUTPUT); // Define o Pino digital
+  pinMode(10,OUTPUT); // Sets the digital pin
   Serial.begin(9600); // BaudRate
 }
 
 void loop()
 {
-if (Serial.available()>0) // Condicao: confere apenas se o sinal esta chegando
+if (Serial.available()>0)                                        // Condition: check only if the signal is coming
   {
-    Serial.readBytes(Buffer,10);   // O sistema sempre monta 10 chars: saida=saida+ " "*(10-len(saida));
-    iCount=String(Buffer).toInt(); // Converte o "string" coletado em um "integer" na variavel iCount
-    if((iCount%nFrames)==0) digitalWrite(10,(iCount/nFrames)%2); // Condicao: Apenas se for um multiplo inteiro de nFrames 
-                                                                 // (neste caso, a resto da divisao deste múltiplo por 2 sera igual 0 ou 1, o que define o estado do pino ("High" ou "Low")
+    Serial.readBytes(Buffer,10);                                 // The system always assembles 10 characters: saida = saida+ " "*(10-len(saida));
+    iCount=String(Buffer).toInt();                               // Convert the collected "string" into an integer -> "iCount" variable
+    if((iCount%nFrames)==0) digitalWrite(10,(iCount/nFrames)%2); // Condition: only integer multiple of nFrames
+                                                                 // (in this case, the  remainder of "integer multiple" by 2 will be equal 1 or 0, which defines the state of the pin ("High" or "Low")
   }
 
   //
